d32e414df0bcefe65b705e9a70b2ea30ed794255
Hossein Saleminejad <h.saleminejad@gmail.com>
2014-12-12 00:22:42 +0330

    Create template.php

---
+++ views/template.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script language="javascript" type="text/javascript" src="/js/ajaxvu.js"></script>
+   <title><?=$title?></title>
+</head>
+<body>
+   <nav>
+      <ul>
+         <li><a class="ajaxvu" href="/">Home</a></li>
+         <li><a class="ajaxvu" href="/index.php/welcome/test">Test</a></li>
+         <li><a href="http://google.com" target="_blank">Google</a></li>
+      </ul>
+   </nav>
+   <div id="content">
+      <?=$content?>
+   </div>
+</body>
+</html>
